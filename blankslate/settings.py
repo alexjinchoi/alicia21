145323b6888fefef33bc7a12235c38f8ff73d2f3
Alex Jin Choi <alexjinchoi@gmail.com>
2017-08-25 16:23:24 +0900

    configured secret_key as variable

--- blankslate/settings.py
+++ blankslate/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
-from .settings_secret import *
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
@@ -31,6 +29,9 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
